48286f95fcfc724afaad55513df5112c992465cc
Irfancpv99 <irfancpv99@gmail.com>
2026-04-15 16:43:26 +0200

    test For Category <Model>

--- src/test/java/com/bookmanager/model/CategoryTest.java
+++ src/test/java/com/bookmanager/model/CategoryTest.java
@@ -8,32 +8,32 @@ class CategoryTest {
 
     @Test
     void testCategoryConstructorAndGetters() {
-        Category category = new Category("cat-1", "Fiction");
+    Category category = new Category("cat-1", "Fiction");
 
-        assertThat(category.getId()).isEqualTo("cat-1");
-        assertThat(category.getName()).isEqualTo("Fiction");
+    assertThat(category.getId()).isEqualTo("cat-1");
+    assertThat(category.getName()).isEqualTo("Fiction");
     }
 
     @Test
     void testToStringReturnsName() {
-        Category category = new Category("cat-1", "Fiction");
+    Category category = new Category("cat-1", "Fiction");
 
-        assertThat(category.toString()).isEqualTo("Fiction");
+    assertThat(category.toString()).isEqualTo("Fiction");
     }
 
     @Test
     void testEqualityById() {
-        Category c1 = new Category("cat-1", "Fiction");
-        Category c2 = new Category("cat-1", "Something Else");
+    Category c1 = new Category("cat-1", "Fiction");
+    Category c2 = new Category("cat-1", "Something Else");
 
-        assertThat(c1).isEqualTo(c2);
+    assertThat(c1).isEqualTo(c2);
     }
 
     @Test
     void testInequalityWithDifferentId() {
-        Category c1 = new Category("cat-1", "Fiction");
-        Category c2 = new Category("cat-2", "Fiction");
+    Category c1 = new Category("cat-1", "Fiction");
+    Category c2 = new Category("cat-2", "Fiction");
 
-        assertThat(c1).isNotEqualTo(c2);
+    assertThat(c1).isNotEqualTo(c2);
     }
 }

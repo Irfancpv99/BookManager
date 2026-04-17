ddd8b75cd80b7d6674f5a31543b2f3a1b956a0ea
Irfancpv99 <irfancpv99@gmail.com>
2026-04-17 16:45:06 +0200

    RED : Implemented first test for book controller

---
+++ src/test/java/com/bookmanager/controller/BookControllerTest.java
@@ -0,0 +1,41 @@
+package com.bookmanager.controller;
+
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.bookmanager.model.Book;
+import com.bookmanager.service.BookService;
+import com.bookmanager.view.BookView;
+
+@ExtendWith(MockitoExtension.class)
+class BookControllerTest {
+
+    @Mock
+    private BookService bookService;
+
+    @Mock
+    private BookView bookView;
+
+    @InjectMocks
+    private BookController bookController;
+
+    private Book book1;
+
+    @BeforeEach
+    void setUp() {
+        book1 = new Book("book-1", "1984", "George Orwell", "cat-1");
+    }
+
+    @Test
+    void allBooks_shouldDelegateToService() {
+        bookController.allBooks();
+
+        verify(bookService).allBooks(bookView);
+    }
+}
